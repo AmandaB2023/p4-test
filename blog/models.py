be0fc20c998ae94e545bbaffd3361b70312f6d34
Amanda Fahy <amandafahy@ymail.com>
2024-09-04 00:32:31 +0000

    Add static images , comments, edit and delete buttons

--- blog/models.py
+++ blog/models.py
@@ -22,8 +22,21 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-from django.db import models
 
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+# Order comments by newest first.
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
 
 class About(models.Model):
     title = models.CharField(max_length=200)
@@ -32,3 +45,4 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
